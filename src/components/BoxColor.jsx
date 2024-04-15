31c38e6dd7c8d7b928d39ded13cedfbf95b1dca7
Adrian <adrian>
2024-04-15 04:51:00 +0200

    done

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,26 @@
-function boxColor ({color, value}) {
+
+import { useRef, useEffect } from 'react';
+
+const BoxColor = ({ color, inputValue }) => {
+  const boxRef = useRef(null);
+  let texto = `Esto no es el ${inputValue}`;
+
+  useEffect(() => {
+    boxRef.current.style.backgroundColor = 'initial'
+    if (color === inputValue) {
+      boxRef.current.style.backgroundColor = color;
+      boxRef.current.textContent = `Esto sí es el ${inputValue}`;
+    } else {
+      boxRef.current.style.backgroundColor = 'none';
+      boxRef.current.textContent = texto;
+    }
+  }, [color,inputValue]);
+
   return (
     <>
-    </> 
-  )
-}
-export default boxColor;  
+    <div className={`box ${color}`} ref={boxRef}></div>
+    </>
+  );
+};
+
+export default BoxColor;
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,10 +1,34 @@
+// MyFormChallenge.jsx
+import React, { useState } from 'react';
+import BoxColor from './BoxColor';
 
+const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
-function MyForm() {
+const MyForm = () => {
+  const [inputValue, setInputValue] = useState('color');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value.toLowerCase());
+  };
 
   return (
-    <></>
+    <>
+    <section>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        placeholder="Choose a color"
+        value={inputValue}
+        
+      />
+      <div className="container">
+        {colors.map((color, index) => (
+          <BoxColor key={index} color={color} inputValue={inputValue} />
+        ))}
+      </div>
+    </section>
+    </>
   );
-}
+};
 
 export default MyForm;
